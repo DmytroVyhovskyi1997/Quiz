a8a13dcb6ae3e3629b341ab16ecc9801792858ed
DmytroVyhovskyi1997 <111079087+DmytroVyhovskyi1997@users.noreply.github.com>
2023-08-24 23:45:06 -0700

    upd styles

--- src/main.js
+++ src/main.js
@@ -1,38 +1,6 @@
-// import { formInput } from "./formInput";
-// import { questions } from "./questions";
+import { formInput } from "./formInput/formInput";
+import { questions } from "./questions/questions";
 
-const questions = [
-  {
-    question: "Яка мова прогамування працює в браузері?",
-    answers: ["Java", "C", "Python", "JavaScript"],
-    correct: 4,
-  },
-  {
-    question: "Що означає CSS?",
-    answers: [
-      "Central Style Sheets",
-      "Cascading Style Sheets",
-      "Cascading Simple Sheets",
-      "Cars SUVs Sailboats",
-    ],
-    correct: 2,
-  },
-  {
-    question: "Що означає HTML?",
-    answers: [
-      "Hypertext Markup Language",
-      "Hypertext Markdown Language",
-      "Hyperloop Machine Language",
-      "Helicopters Terminals Motorboats Lamborginis",
-    ],
-    correct: 1,
-  },
-  {
-    question: "В якому році був створений JavaScript?",
-    answers: ["1996", "1995", "1994", "все ответы неверные"],
-    correct: 2,
-  },
-];
 const headerContainer = document.querySelector("#header");
 const listContainer = document.querySelector("#list");
 const submitBtn = document.querySelector("#submit");
@@ -55,7 +23,7 @@ function goBackButton() {
     clearPage();
     showQuestions();
   }
-}
+};
 
 function showQuestions() {
   const headerTemplate = `<h2 class="title">%title%</h2>`;
@@ -72,7 +40,7 @@ function showQuestions() {
 	  <li>
      <label>
 	  <input value='%number%' type="radio" class="answer" name="answer" />
-	  <span>%answer%</span>
+	  <span>%answer% </span>
      </label>
     </li>`;
     let answerHtml = questionsTemplate.replace("%answer%", answerText);
@@ -81,7 +49,7 @@ function showQuestions() {
     listContainer.innerHTML += answerHtml;
     answerNumber++;
   }
-}
+};
 
 function checkAnswer() {
   const checkedRadio = listContainer.querySelector(
@@ -106,7 +74,7 @@ function checkAnswer() {
     clearPage();
     showResults();
   }
-}
+};
 
 function showResults() {
   const resultsTemplate = `
@@ -140,115 +108,14 @@ function showResults() {
   submitBtn.onclick = () => history.go();
 
   formInput(formContainer);
-}
+};
 
 function clearPage() {
   headerContainer.innerHTML = "";
   listContainer.innerHTML = "";
-}
+};
 
 
 
 
 
-// import Notiflix from 'notiflix';
-
- function formInput(formContainer) {
-    const formTemplate = `
-      <div class="box">
-        <form class="form" id="contact-form">
-          <input class="input" type="text" name="fname" id="fname" placeholder="First Name">
-          <input class="input" type="text" name="lname" id="lname" placeholder="Last Name">
-          <input class="input" type="email" name="email" id="email" placeholder="Email">
-          <input class="input" type="tel" name="phone" id="phone" placeholder="Phone">
-          <button class="button" type="submit">Submit</button>
-        </form>
-      </div>
-    `;
-    formContainer.innerHTML = formTemplate;
-  
-    $.validator.addMethod(
-      "regex",
-      function (value, element, regexp) {
-        return this.optional(element) || regexp.test(value);
-      },
-      "Введено неправильний формат."
-    );
-    const phoneInput = document.getElementById('phone');
-
-  const iti = window.intlTelInput(phoneInput, {
-    separateDialCode: true,
-    initialCountry: 'auto',
-    preferredCountries: ['us', 'gb', 'ca'],
-    utilsScript: 'https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.12/js/utils.js'
-  });
-  $(phoneInput).mask('000-000-0000');
-  
-    $("#contact-form").validate({
-      rules: {
-        fname: {
-          required: true,
-          minlength: 3,
-          regex: /^[A-Za-z\s]+$/,
-        },
-        lname: {
-          required: true,
-          minlength: 3,
-          regex: /^[A-Za-z\s]+$/,
-        },
-        email: {
-          required: true,
-          email: true,
-
-        },
-        phone: {
-          minlength: 10,
-          required: true,
-        },
-      },
-      messages: {
-        fname: {
-          required: "Це поле обов'язкове для заповнення",
-          minlength: "Мінімум 3 символи",
-          regex: "Введіть тільки літери та пробіли",
-        },
-        lname: {
-          required: "Це поле обов'язкове для заповнення",
-          minlength: "Мінімум 3 символи",
-          regex: "Введіть тільки літери та пробіли",
-        },
-        email: {
-          required: "Це поле обов'язкове для заповнення",
-          email: "Введіть коректну адресу електронної пошти",
-        },
-        phone: {
-          required: "Це поле обов'язкове для заповнення",
-        },
-      },
-      submitHandler: function (form, e) {
-        e.preventDefault();
-        const fname = $("#fname").val();
-        const lname = $("#lname").val();
-        const email = $("#email").val();
-        const phone = $("#phone").val();
-  
-        $.ajax({
-          url: "./process_form.php",
-          type: "POST",
-          data: {
-            fname: fname,
-            lname: lname,
-            email: email,
-            phone: phone,
-          },
-          success: function (response) {
-            alert(response.message);
-            form.reset();
-          },
-          error: function (xhr, status, error) {
-            alert("Помилка при відправці форми");
-          },
-        });
-      },
-    });
-  }
---
+++ src/questions/questions.js
@@ -0,0 +1,32 @@
+export const questions = [
+    {
+      question: "Яка мова прогамування працює в браузері?",
+      answers: ["Java", "C", "Python", "JavaScript"],
+      correct: 4,
+    },
+    {
+      question: "Що означає CSS?",
+      answers: [
+        "Central Style Sheets",
+        "Cascading Style Sheets",
+        "Cascading Simple Sheets",
+        "Cars SUVs Sailboats",
+      ],
+      correct: 2,
+    },
+    {
+      question: "Що означає HTML?",
+      answers: [
+        "Hypertext Markup Language",
+        "Hypertext Markdown Language",
+        "Hyperloop Machine Language",
+        "Helicopters Terminals Motorboats Lamborginis",
+      ],
+      correct: 1,
+    },
+    {
+      question: "В якому році був створений JavaScript?",
+      answers: ["1996", "1995", "1994", "все ответы неверные"],
+      correct: 2,
+    },
+  ];
